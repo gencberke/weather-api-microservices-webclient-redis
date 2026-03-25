11d15d7411f581ca5bc09cd0668cb89295a69636
gencberke <berkegenc.dev@gmail.com>
2026-03-25 15:29:26 +0300

    feat: user-service mappers created

---
+++ user-service/src/main/java/com/berkedev/weather/user/data/mapper/FavoriteCityMapper.java
@@ -0,0 +1,13 @@
+package com.berkedev.weather.user.data.mapper;
+
+import com.berkedev.weather.user.data.dto.response.FavoriteCityResponse;
+import com.berkedev.weather.user.data.entity.FavoriteCity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface FavoriteCityMapper {
+
+    @Mapping(target = "email", expression = "java(favoriteCity.getUser().getEmail())")
+    FavoriteCityResponse toResponse(FavoriteCity favoriteCity);
+}
